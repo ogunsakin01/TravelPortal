194442a34cef781430f376eb0e7663039f268285
mazeedbash <bashorun.mazeed@gmail.com>
2018-04-09 11:25:28 +0100

    Mazeed

---
+++ resources/views/partials/css.blade.php
@@ -0,0 +1,15 @@
+<!-- STYLES -->
+<link href="css/animate.min.css" rel="stylesheet">
+<link href="css/bootstrap-select.min.css" rel="stylesheet">
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/owl-carousel-theme.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/flexslider.css" rel="stylesheet" media="screen">
+<link href="css/style.css" rel="stylesheet" media="screen">
+<!-- LIGHT -->
+<link rel="stylesheet" type="text/css" href="assets/css/dummy.css" id="select-style">
+<link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- FONTS -->
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600' rel='stylesheet' type='text/css'>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+ <div class="row transparent-menu-top">
+        <div class="container clear-padding">
+            <div class="navbar-contact">
+                <div class="col-md-7 col-sm-6 clear-padding">
+                    <a href="#" class="transition-effect"><i class="fa fa-phone"></i> (+91) 1234567890</a>
+                    <a href="#" class="transition-effect"><i class="fa fa-envelope-o"></i> support@email.com</a>
+                </div>
+                <div class="col-md-5 col-sm-6 clear-padding search-box">
+                    <div class="col-md-6 col-xs-5 clear-padding">
+                        <form>
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" required placeholder="Search">
+                                <span class="input-group-addon"><i class="fa fa-search fa-fw"></i></span>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-6 col-xs-7 clear-padding user-logged">
+                        <a href="#" class="transition-effect">
+                            <img src="assets/images/user.jpg" alt="cruise">
+                            Hi, Lenore
+                        </a>
+                        <a href="#" class="transition-effect">
+                            <i class="fa fa-sign-out"></i>Sign Out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
